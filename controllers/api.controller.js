b7d83bcca8ee41b18138ccae886dcc378ed455c6
Mark <mark@Marks-MacBook-Pro.local>
2024-08-25 23:33:00 +0100

    This adds the GET /api/articles/:article__id endpoint and also adds in further testing and error handling

--- controllers/api.controller.js
+++ controllers/api.controller.js
@@ -1,14 +1,25 @@
-const { end } = require("../db/connection");
-const {selectTopics,serveApi} = require("../models/api.model")
+// const { end } = require("../db/connection");
+const {selectTopics,selectArticleById} = require("../models/api.model")
 // const {errorHandlers} = require('../errors')
 
 exports.getTopics = (req, res, next) => {
     selectTopics().then((topics)=>{
         res.status(200).send({topics})
+    }).catch((err)=> {
+        console.log(err)
+        next(err)
+})}
+
+exports.getArticleById = (req, res, next) => {
+    const { article_id } = req.params;
+    selectArticleById(article_id).then((article) => {
+      res.status(200).send({ article });
+    }).catch((err)=> {
+      next(err)
     })
-}
+  };
 
 exports.endpoints = (req, res, next) => {
     const endpoints = require('../endpoints.json'); // require automatically parses
     res.status(200).json(endpoints)
-}
+} // this does not need an error block as it can only ever respond on this end point
--- models/api.model.js
+++ models/api.model.js
@@ -8,3 +8,18 @@ return db.query(`SELECT * FROM topics;`).then((result)=>{
     return result.rows
 })
 }
+
+exports.selectArticleById = (article_id) => {
+    return db
+      .query('SELECT * FROM articles WHERE article_id = $1;', [article_id])
+      .then((result) => {
+        console.log(result.rows)
+        if (result.rows.length === 0) {
+          return Promise.reject({
+            status: 404,
+            msg: 'article does not exist'
+          })
+        }
+        return result.rows[0];
+      });
+    }
